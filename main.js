0c622559b92ecefbcc2b5fe732330dcbb1ec523a
Italo Paula <italo76g@gmail.com>
2023-05-21 09:29:07 -0300

    editing js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function createMemory (date, title, image, descricao) {
+function createMemory (link, date, title, image, description) {
     return `
-        <a href="https://www.youtube.com/watch?v=3nz5HkUJaVU" target="_blank" class="memory">
+        <a href="https://github.com/italoopaula/${link}" target="_blank" class="memory">
         <small>${date}</small>
         <h1>${title}</h1>
         <div>
             <img src="./assets/${image}.png" alt="Imagem do evento ${image}">
         </div>
-        <p>${descricao}</p>
+        <p>${description}</p>
         </a>
     `
 }
 
 document.querySelector("nav").innerHTML =
-    createMemory('06 de Novembro de 2022', 'NLW Copa', 'nlw-copa', 'Na NLW Copa desenvolvemos um projeto que mostra o calendário de jogos da Copa do Mundo de 2022.') +
-    createMemory('21 de Janeiro de 2023', 'NLW Setup', 'nlw-setup', 'Na NLW Setup desenvolvemos um app para ajudar a rastrear os hábitos.')
+    createMemory('nlw-copa', '06 de Novembro de 2022', 'NLW Copa', 'nlw-copa', 'Na NLW Copa desenvolvemos um projeto que mostra o calendário de jogos da Copa do Mundo de 2022.') +
+    createMemory('nlw-setup', '21 de Janeiro de 2023', 'NLW Setup', 'nlw-setup', 'Na NLW Setup desenvolvemos um app para ajudar a rastrear os hábitos.')
